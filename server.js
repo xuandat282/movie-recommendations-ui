df517b6f20c23f0b44b7e295ba60a47a65b42a19
Xuan Dat <xuandat282@gmail.com>
2023-05-10 17:06:57 +0700

    a

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/movie-recommendations-ui'));
+app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-res.sendFile('src/index.html', {root: 'dist/angular-heroku/'}),
+  res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
